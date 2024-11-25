962d05f0d960476ff7a70a806d6a5debea97287b
hoaiphuc <nguyenhoaiphuc3122001@gmail.com>
2024-11-25 13:31:59 +0700

    add image for manager

--- app/manager/page.tsx
+++ app/manager/page.tsx
@@ -1,8 +1,16 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Page = () => {
     return (
-        <div>Page</div>
+        <div style={{ width: '100%', height: '100%', position: 'relative' }}>
+            <Image
+                alt='Mountains'
+                src='/manager.png'
+                width={2000}
+                height={2000}
+            />
+        </div>
     )
 }
 
