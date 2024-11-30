5cb8d96c79242475445fba2b836b89997e16e057
Cassme1202 <NLDTan120201@gmail.com>
2024-11-30 21:20:34 +0700

    update admin

--- app/lib/navbarAdmin.tsx
+++ app/lib/navbarAdmin.tsx
@@ -2,8 +2,8 @@ import { SideNavItem } from "@/app/constants/types/homeType";
 import {
   faClipboardList,
   faHouse,
+  faSliders,
   faUser,
-  // faSliders,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
@@ -13,6 +13,7 @@ export const navbarAdmin: SideNavItem[] = [
     path: "/admin",
     icon: <FontAwesomeIcon icon={faHouse} className="w-7 h-7 " />,
   },
+
   {
     title: "Người dùng",
     path: "/admin/user",
@@ -22,5 +23,10 @@ export const navbarAdmin: SideNavItem[] = [
     title: "Cài đặt dịch vụ",
     path: "/admin/service",
     icon: <FontAwesomeIcon icon={faClipboardList} className="w-7 h-7" />,
+  },
+  {
+    title: "Cài đặt hệ thống",
+    path: "/admin/setting",
+    icon: <FontAwesomeIcon icon={faSliders} className="w-7 h-7" />,
   },
 ];
