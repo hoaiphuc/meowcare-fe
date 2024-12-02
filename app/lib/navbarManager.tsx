5cdeae48734d7e4f89da8cb88beba2df1b540111
Cassme1202 <NLDTan120201@gmail.com>
2024-12-03 00:39:30 +0700

    code manage withdraw

--- app/lib/navbarManager.tsx
+++ app/lib/navbarManager.tsx
@@ -1,53 +1,49 @@
 import { SideNavItem } from "@/app/constants/types/homeType";
 import {
-    faFile,
-    faFlag,
-    faHouse,
-    faMoneyBillTransfer,
-    faRectangleList,
-    faUser,
+  faFile,
+  faFlag,
+  faHouse,
+  faMoneyBillTransfer,
+  faRectangleList,
+  faUser,
+  faCoins,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 export const navbarManager: SideNavItem[] = [
-    {
-        title: "Trang chủ",
-        path: "/manager",
-        icon: <FontAwesomeIcon icon={faHouse} className="w-7 h-7 " />,
-    },
-    {
-        title: "Đơn đăng ký",
-        path: "/manager/registrationform",
-        icon: (
-            <FontAwesomeIcon icon={faFile} className="w-7 h-7 " />
-        ),
-    },
-    {
-        title: "Người dùng",
-        path: "/manager/user",
-        icon: (
-            <FontAwesomeIcon icon={faUser} className="w-7 h-7 " />
-        ),
-    },
-    {
-        title: "Bài kiểm tra",
-        path: "/manager/quiz",
-        icon: (
-            <FontAwesomeIcon icon={faRectangleList} className="w-7 h-7" />
-        ),
-    },
-    {
-        title: "Báo cáo",
-        path: "/manager/report",
-        icon: (
-            <FontAwesomeIcon icon={faFlag} className="w-7 h-7 " />
-        ),
-    },
-    {
-        title: "Giao dịch",
-        path: "/manager/transaction",
-        icon: (
-            <FontAwesomeIcon icon={faMoneyBillTransfer} className="w-7 h-7" />
-        ),
-    },
+  {
+    title: "Trang chủ",
+    path: "/manager",
+    icon: <FontAwesomeIcon icon={faHouse} className="w-7 h-7 " />,
+  },
+  {
+    title: "Đơn đăng ký",
+    path: "/manager/registrationform",
+    icon: <FontAwesomeIcon icon={faFile} className="w-7 h-7 " />,
+  },
+  {
+    title: "Người dùng",
+    path: "/manager/user",
+    icon: <FontAwesomeIcon icon={faUser} className="w-7 h-7 " />,
+  },
+  {
+    title: "Bài kiểm tra",
+    path: "/manager/quiz",
+    icon: <FontAwesomeIcon icon={faRectangleList} className="w-7 h-7" />,
+  },
+  {
+    title: "Báo cáo",
+    path: "/manager/report",
+    icon: <FontAwesomeIcon icon={faFlag} className="w-7 h-7 " />,
+  },
+  {
+    title: "Giao dịch",
+    path: "/manager/withdrawal",
+    icon: <FontAwesomeIcon icon={faCoins} className="w-7 h-7" />,
+  },
+  {
+    title: "Giao dịch",
+    path: "/manager/transaction",
+    icon: <FontAwesomeIcon icon={faMoneyBillTransfer} className="w-7 h-7" />,
+  },
 ];
